990400bfeccfc4c32db0e5e6bff6d2ef2d5ed3ff
Max <maksgalochkin2@gmail.com>
2022-09-29 18:47:46 +0300

    navigate

--- test/src/FormData/AjaxAttr.js
+++ test/src/FormData/AjaxAttr.js
@@ -29,75 +29,42 @@ function openForm(href, target, enctype, nameValues) {
     form.remove();
 }
 
-//receives: [ [key, value], [key2, value2], [key, value2] ]. An entry map.
-
-function formDataToEncodedUri(formData, base, href) {
-    const url = new URL(href, base);
-    if (!formData)
-        return url;
-    for (let [k, v] of formData.entries()) {
-        if (!(v instanceof String) && typeof v !== "string")
-            throw TypeError("FormData with File/Blob entities cannot be encoded to uriComponent here.");
-        url.searchParams.set(k, v);
-    }
-    return url;
+function GETAttr(e, returnType = "_self") {
+    const entries = e.detail;
+    const url = new URL(this.value, location);
+    if (entries)
+        for (let [k, v] of entries)
+            url.searchParams.set(k, v);
+    if (returnType === "text" || returnType === "json")
+        doFetchAndEvents(this.ownerElement, url, null, "GET", returnType);
+    else if (["_self", "_blank", "_parent", "_top"].includes(returnType))
+        open(url, returnType);
+    else if (returnType === "history")
+        history.pushState({}, null, url), window.dispatchEvent(new Event("popstate"));
 }
 
-export const GetFormDataJSON = function (e) {
-    const formData = e.detail;
-    const url = formDataToEncodedUri(formData, location, this.value);
-    doFetchAndEvents(this.ownerElement, url, null, "GET", "json");
+function POSTFormDataAttr(e, returnType) {             //formdata is only useful for POST
+    const entries = e.detail;
+    const url = new URL(this.value);
+    const formData = new FormData();
+    for (let [name, value] of entries)
+        formData.append(name, value);
+    for (let [k, v] of url.searchParams.entries())
+        formData.append(k, v);
+    for (let k of url.searchParams.keys())
+        url.searchParams.delete(k);
+    doFetchAndEvents(this.ownerElement, url, formData, "POST", returnType);
 }
 
-export class GetFormDataText extends Attr {
-    onEvent({detail: formData}) {
-        const url = formDataToEncodedUri(formData, location, this.value);
-        doFetchAndEvents(this.ownerElement, url, null, "GET", "text");
-    }
-}
-
-function GETAttr(returnType = "_self") {
-    return class AjaxAttr extends Attr {
-        onEvent({detail: formData}) {
-            const url = formDataToEncodedUri(formData, location, this.value);
-            if (returnType === "text" || returnType === "json")
-                doFetchAndEvents(this.ownerElement, url, null, "GET", returnType);
-            else if (["_self", "_blank", "_parent", "_top"].includes(returnType))
-                open(url, returnType);
-            else if (returnType === "history")
-                history.pushState({}, null, url), window.dispatchEvent(new Event("popstate"));
-        }
-    }
-}
-
-function POSTFormDataAttr(returnType) {             //formdata is only useful for POST
-    return class AjaxAttr extends Attr {
-        onEvent({detail: entries}) {
-            const url = new URL(this.value);
-            const formData = new FormData();
-            for (let [name, value] of entries)
-                formData.append(name, value);
-            for (let [k, v] of url.searchParams.entries())
-                formData.append(k, v);
-            for (let k of url.searchParams.keys())
-                url.searchParams.delete(k);
-            doFetchAndEvents(this.ownerElement, url, formData, "POST", returnType);
-        }
-    }
-}
-
-function POSTUrlEncodedAttr(returnType) {
-    return class AjaxAttr extends Attr {
-        onEvent({detail: entries}) {
-            const url = new URL(this.value);
-            for (let [k, v] of entries)
-                url.searchParams.append(k, v);
-            const body = url.searchParams.toString();
-            for (let k of url.searchParams.keys())
-                url.searchParams.delete(k);
-            doFetchAndEvents(this.ownerElement, url, body, "post", returnType);
-        }
-    }
+function POSTUrlEncodedAttr(e, returnType) {
+    const entries = e.detail;
+    const url = new URL(this.value);
+    for (let [k, v] of entries)
+        url.searchParams.append(k, v);
+    const body = url.searchParams.toString();
+    for (let k of url.searchParams.keys())
+        url.searchParams.delete(k);
+    doFetchAndEvents(this.ownerElement, url, body, "post", returnType);
 }
 
 function POSTAttr(target = "_self", enctype = "application/x-www-form-urlencoded") {
@@ -114,19 +81,42 @@ function POSTAttr(target = "_self", enctype = "application/x-www-form-urlencoded
 
 //method_target_enctype_Attr
 
-export const GET_Attr = GETAttr();
-export const GET_text_Attr = GETAttr("text");
-export const GET_json_Attr = GETAttr("json");
-export const GET_history_Attr = GETAttr("history");
-export const GET__blank_Attr = GETAttr("_blank");
-export const GET__parent_Attr = GETAttr("_parent");
-export const GET__top_Attr = GETAttr("_top");
+export const GET_Attr = function (e) {
+    GETAttr.call(this, e);
+}
 
-export const POST_text_formdata_Attr = POSTFormDataAttr("text");
-export const POST_json_formdata_Attr = POSTFormDataAttr("json");
+// export const GET_text_Attr = GETAttr("text");
+export const GET_json_Attr = function (e) {
+    GETAttr.call(this, e, "json");
+}
 
-export const POST_text_uriComponent_Attr = POSTUrlEncodedAttr("text");
-export const POST_json_uriComponent_Attr = POSTUrlEncodedAttr("json");
+export const GET_history_Attr = function (e) {
+    GETAttr.call(this, e, "history");
+}
+
+export const GET__blank_Attr = function (e) {
+    GETAttr.call(this, e, "_blank");
+}
+export const GET__parent_Attr = function (e) {
+    GETAttr.call(this, e, "_parent");
+}
+export const GET__top_Attr = function (e) {
+    GETAttr.call(this, e, "_top");
+}
+
+export const POST_text_formdata_Attr = function (e) {
+    POSTFormDataAttr.call(this, e, "text");
+}
+export const POST_json_formdata_Attr = function (e) {
+    POSTFormDataAttr.call(this, e, "json");
+}
+
+export const POST_text_uriComponent_Attr = function (e) {
+    POSTUrlEncodedAttr.call(this, e, "text");
+}
+export const POST_json_uriComponent_Attr = function (e) {
+    POSTUrlEncodedAttr.call(this, e, "json");
+}
 
 //todo the Post form data are untested.
 export const POST_uriComponent_Attr = POSTAttr();
