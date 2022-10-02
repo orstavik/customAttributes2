16a3a5e9daabef31684745c9a29fa7a5995cc6ea
intertext.no <orstavik77@gmail.com>
2022-10-02 09:29:55 +0200

    babystep

--- src/EventRegistry.js
+++ src/EventRegistry.js
@@ -26,63 +26,130 @@ Object.defineProperties(Attr.prototype, {
 });
 
 class NativeBubblingAttribute extends Attr {
+  // static #reroute = function (e) {
+  //   // e.preventDefault(); // if dispatchEvent propagates sync, native defaultActions can still be used.
+  //   e.stopImmediatePropagation();
+  //   customEvents.dispatch(e, e.composedPath()[0]);
+  // };
+  //
+  // upgrade() {
+  //   this.ownerElement.addEventListener(this.constructor.prefix, NativeBubblingAttribute.#reroute);
+  // }
+  //
+  // destructor() {
+  //   for (let o of this.ownerElement.attributes)
+  //     if (this.constructor === o.constructor && o !== this)
+  //       return;
+  //   this.ownerElement.removeEventListener(this.constructor.prefix, NativeBubblingAttribute.#reroute);
+  // }
+  //
+  static subclass(prefix) {
+    if (!(`on${prefix}` in HTMLElement.prototype && `on${prefix}` in window))
+      return;
+    return {prefix, suffix: "", Definition: NativeBubblingEvent};
+    // return class NativeBubblingAttributeImpl extends NativeBubblingAttribute {
+    //   static get prefix() {
+    //     return prefix;
+    //   }
+    //
+    //   static get suffix() {
+    //     return "";
+    //   }
+    // };
+  }
+}
+
+class NativeBubblingEvent extends Attr {
   static #reroute = function (e) {
     // e.preventDefault(); // if dispatchEvent propagates sync, native defaultActions can still be used.
     e.stopImmediatePropagation();
     customEvents.dispatch(e, e.composedPath()[0]);
   };
 
-  upgrade() {
-    this.ownerElement.addEventListener(this.constructor.prefix, NativeBubblingAttribute.#reroute);
+  upgrade(prefix) { //todo make suffix ...variadic
+    this.ownerElement.addEventListener(this._prefix = prefix, NativeBubblingEvent.#reroute);
   }
 
   destructor() {
     for (let o of this.ownerElement.attributes)
-      if (this.constructor === o.constructor && o !== this)
+      if (this.constructor === o.constructor && o !== this && this._prefix === o._prefix)
         return;
-    this.ownerElement.removeEventListener(this.constructor.prefix, NativeBubblingAttribute.#reroute);
-  }
-
-  static subclass(prefix) {
-    if (!(`on${prefix}` in HTMLElement.prototype && `on${prefix}` in window))
-      return;
-    return class NativeBubblingAttributeImpl extends NativeBubblingAttribute {
-      static get prefix() {
-        return prefix;
-      }
-
-      static get suffix() {
-        return "";
-      }
-    };
+    this.ownerElement.removeEventListener(this._prefix, NativeBubblingEvent.#reroute);
   }
 }
 
+class NativeDocumentOnlyEvent extends Attr {
+  upgrade(prefix) {
+    const owner = new WeakRef(this);
+    const reroute = function (e) {
+      const target = owner.deref();
+      target ?
+        customEvents.dispatch(e, target) :
+        document.removeEventListener(prefix, reroute);
+    }
+    document.addEventListener(prefix, reroute);
+  }
+}
+
+class NativeWindowOnlyEvent extends Attr {
+  upgrade(prefix) {
+    const owner = new WeakRef(this);
+    const reroute = function (e) {
+      const target = owner.deref();
+      target && target.ownerElement ?   //remove the attribute from the dom or from the ownerElement, then it is lost.
+        customEvents.dispatch(e, target) :
+        window.removeEventListener(prefix, reroute);
+    }
+    window.addEventListener(prefix, reroute);
+  }
+}
+
+//
+// class NativeDomEvent extends Attr{
+//   static #reroute = function (e) {
+//     // e.preventDefault(); // if dispatchEvent propagates sync, native defaultActions can still be used.
+//     e.stopImmediatePropagation();
+//     customEvents.dispatch(e, e.composedPath()[0]);
+//   };
+//
+//   upgrade(prefix) {
+//     this.prefix = prefix;
+//     this.ownerElement.addEventListener(prefix, NativeDomEvent.#reroute);
+//   }
+//
+//   destructor() {
+//     // for (let o of this.ownerElement.attributes)
+//     //   if (this.constructor === o.constructor && o !== this)
+//     //     return;
+//     this.ownerElement.removeEventListener(this.prefix, NativeDomEvent.#reroute);
+//   }
+// }
 
 class NativeNonBubblingAttribute extends Attr {
   static subclass(prefix) {
     if (!(`on${prefix}` in window) || `on${prefix}` in HTMLElement.prototype)
       return;
-    return class NativeNonBubblingAttributeImpl extends Attr {
-
-      static #rerouter = function reroute(e) {
-        customEvents.dispatch(e);
-        if (!customEvents.count(e.type))
-          window.removeEventListener(this.prefix, this.#rerouter);
-      }.bind(this);
-
-      upgrade() {
-        window.addEventListener(this.constructor.prefix, this.constructor.#rerouter);
-      }
-
-      static get prefix() {
-        return prefix;
-      }
-
-      static get suffix() {
-        return "";
-      }
-    };
+    return {prefix, suffix: "", Definition: NativeWindowOnlyEvent};
+    //   return class NativeNonBubblingAttributeImpl extends Attr {
+    //
+    //     static #rerouter = function reroute(e) {
+    //       customEvents.dispatch(e);
+    //       if (!customEvents.count(e.type))
+    //         window.removeEventListener(this.prefix, this.#rerouter);
+    //     }.bind(this);
+    //
+    //     upgrade() {
+    //       window.addEventListener(this.constructor.prefix, this.constructor.#rerouter);
+    //     }
+    //
+    //     static get prefix() {
+    //       return prefix;
+    //     }
+    //
+    //     static get suffix() {
+    //       return "";
+    //     }
+    //   };
   }
 }
 
@@ -112,36 +179,14 @@ class EventRegistry {
   static makeSuffixDefinition(aCustomAttrDefinition, prefix, suffix) {
     if (suffix[0] === "_")
       suffix = suffix.substring(1).split("_");
-    return class SuffixedCustomAttr extends aCustomAttrDefinition {
-      static get prefix() {
-        return prefix;
-      }
-
-      static get suffix() {
-        return suffix;
-      }
-    };
+    return {Definition: aCustomAttrDefinition, prefix, suffix};
   }
 
   define(prefix, Class) {
     const overlapDefinition = this.prefixOverlaps(prefix);
     if (overlapDefinition)
       throw `The customEvent "${prefix}" is already defined as "${overlapDefinition}".`;
-    if (Class.prefix)
-      throw `${Class.name} definition is already used (${Class.name}.prefix === "${Class.prefix}"). 
-    What about 'customEvents.define("${prefix}", class Something extends ${Class.name}{});'?`;
-    Object.defineProperties(Class, {
-      "prefix": {
-        get: function () {
-          return prefix;
-        }
-      }, "suffix": {
-        get: function () {
-          return "";
-        }
-      }
-    });
-    this[prefix] = Class;
+    this[prefix] = {prefix, suffix: "", Definition: Class};
     this.#upgradeUnknownEvents(prefix, Class);
   }
 
@@ -153,7 +198,7 @@ class EventRegistry {
 
   suffixDefinition(name) {
     const prefix = Object.keys(this).find(prefix => this[prefix] && name.startsWith(prefix));
-    return prefix && EventRegistry.makeSuffixDefinition(this[prefix], prefix, name.substring(prefix.length));
+    return prefix && EventRegistry.makeSuffixDefinition(this[prefix].Definition, prefix, name.substring(prefix.length));
   }
 
   upgrade(...attrs) {
@@ -169,10 +214,10 @@ class EventRegistry {
     }
   }
 
-  #upgradeAttribute(at, Definition) {
+  #upgradeAttribute(at, {Definition, suffix, prefix}) {
     Object.setPrototypeOf(at, Definition.prototype);
     try {
-      at.upgrade?.(Definition.prefix, Definition.suffix);
+      at.upgrade?.(prefix, suffix);
       at.changeCallback?.();
     } catch (error) {
       at.ownerElement.dispatchEvent(new ErrorEvent("error", {
@@ -185,10 +230,10 @@ class EventRegistry {
     }
   }
 
-  count(name) {              //todo this doesn't work.
-    return this.#allAttributes[name].length;
-  }
-
+  // count(name) {              //todo this doesn't work.
+  //   return this.#allAttributes[name].length;
+  // }
+  //
   prefixOverlaps(newPrefix) {
     for (let oldPrefix in this)
       if (this[oldPrefix] && (newPrefix.startsWith(oldPrefix) || oldPrefix.startsWith(newPrefix)))
@@ -245,13 +290,16 @@ class EventRegistry {
             attr.ownerElement.removeAttribute(attr.name);//todo 1.
         }
         //broadcast and single-attribute propagation
-      } else {
-        const attributes = target instanceof Attr ? [target] : this.#allAttributes[event.type];
-        for (let attr of attributes) {
-          this.callFilterImpl(attr.allFunctions, attr, event);
-          if (attr.once)
-            attr.ownerElement.removeAttribute(attr.name);
-        }
+      } else if (target instanceof Attr) {
+        // const attributes = target instanceof Attr ? [target] : this.#allAttributes[event.type];
+        // for (let attr of attributes) {
+        // if (!attr.ownerElement.isConnected)   //todo do we want this? or do we not want this? elements off the dom feels wrong.
+        //   continue;
+        const attr = target;
+        this.callFilterImpl(attr.allFunctions, attr, event);
+        if (attr.once)
+          attr.ownerElement.removeAttribute(attr.name);
+        // }
       }
       this.#eventLoop.shift();
     }
